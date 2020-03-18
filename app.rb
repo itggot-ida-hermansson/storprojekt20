71a206a397b6058c84bf2b50cdc91bb944daa2d8
itggot-ida-hermansson <43438175+itggot-ida-hermansson@users.noreply.github.com>
2020-03-18 22:19:25 +0100

    .

--- app.rb
+++ app.rb
@@ -2,18 +2,32 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 
-get('/') do
-    slim(:index)
+
+def connect_to_db(path)
+    db = SQLite3::Database.new(path)
+    db.results_as_hash = true
+    return db
 end
+
+   
+get('/') do
+    slim(:index,locals:{user:'',users:[], message:'Användarnamn'})
+end
+
+
 
 post('/login') do
     userId=params[:user]
     pwd=params[:pass]
 
-    db = SQLite3::Database.new("db/hej.db")
-    db.results_as_hash = true
+    db = connect_to_db("db/hej.db")
+
     user = db.execute("SELECT * FROM user where userid=?", userId)
-    allUser = db.execute("SELECT * FROM user")
-    slim(:start, locals:{user:user,users:allUser})
+    if user == nil or user.length() == 0
+        slim(:index,locals:{user:'',users:[], message:'Fel användare eller passord'})
+    else  
+        allUser = db.execute("SELECT * FROM user where userid!=?", userId)
+        slim(:start, locals:{user:user.first,users:allUser})
+    end    
   end
 

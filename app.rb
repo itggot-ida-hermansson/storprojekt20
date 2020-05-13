4645303a04b1d3ea87abf64c284bf8a5ade16004
itggot-ida-hermansson <43438175+itggot-ida-hermansson@users.noreply.github.com>
2020-05-13 20:00:27 +0200

    .

--- app.rb
+++ app.rb
@@ -44,29 +44,22 @@ enable :sessions
 post('/login') do
     userId=params[:user]
     pwd=params[:pass]
-    puts pwd
-    puts userId
     db = connect_to_db("db/hej.db")
     userResult = db.execute("SELECT * FROM user where userid=?", userId)
-    if userResult
+    if userResult.empty?
+        slim(:index,locals:{user:'',users:[], message:'Hittar inte användare',showGrid:false}) 
+    else 
         user = userResult[0]
-        if user
-            password_digest = BCrypt::Password.new (user["pass"])
-          #  password_digest = user["pass"]
-            if password_digest != pwd
-                puts user['pass']
-                slim(:index,locals:{user:'',users:[], showGrid:false, message:'Fel användare eller passord'})   
-            else
-                session[:hejUser] = user['id']
-                allUser = db.execute("SELECT * FROM user where userid!=?", userId)    
-                slim(:start, locals:{user:user,users:allUser, showGrid:true, message:''})
-            end 
+        password_digest = BCrypt::Password.new (user["pass"])
+
+        if password_digest != pwd
+            slim(:index,locals:{user:'',users:[], showGrid:false, message:'Felaktigt passord'})   
         else
-            slim(:index,locals:{user:'',users:[], message:'Hittar inte användare',showGrid:false})    
-        end
-    else
-        slim(:index,locals:{user:'',users:[], message:'Fel användare eller passord',showGrid:false})    
-    end    
+            session[:hejUser] = user['id']
+            allUser = db.execute("SELECT * FROM user where userid!=?", userId)    
+            slim(:start, locals:{user:user,users:allUser, showGrid:true, message:''})
+        end 
+    end     
 end
 
 get('/users/new') do 
@@ -83,9 +76,16 @@ post('/users') do
 
     db = connect_to_db("db/hej.db")
     result = db.execute("SELECT MAX(id) as id FROM user;")
-    first_result = result[0]
-    id=first_result['id'];
-    id=id+1;  
+    if result.empty? || result[0]['id'] == nil
+        # Finns ingen user i databasen, använd id 1
+        id = 1
+    else
+        # Skapa ny user med första lediga id
+        
+        first_result = result[0]
+        id=first_result['id']
+        id=id+1  
+    end 
     password_digest = BCrypt::Password.create(pwd)
     db.execute("INSERT INTO user('id', 'userid', 'name', 'country', 'pass', 'adress1', 'adress2') VALUES (?,?,?,?,?,?,?);", id,userId,name,country,password_digest,adress1,adress2);
     slim(:index,locals:{user:'',users:[], message:'Användarnamn', showGrid:false})
@@ -93,18 +93,18 @@ end
 
 enable :sessions
 get('/users/:id/edit') do 
-    #id = session[:hejUser]
+    # Skickar id som en del av url:en för att testa det också 
     id = params[:id]
     db = connect_to_db("db/hej.db")
     userResult = db.execute("SELECT * FROM user WHERE id=?", id)
     user = userResult[0]
-    if user
-        slim(:"users/edit",locals:{user:user,users:[], message:'', showGrid:false})
-    end
+    
+    slim(:"users/edit",locals:{user:user,users:[], message:'', showGrid:false})
 end
 
 enable :sessions
 post('/user/save') do
+    # Hämtar id från sessionen 
     id = session[:hejUser]
     pwd=params[:pass]
     userId=params[:userid]
@@ -125,41 +125,41 @@ end
 
 enable :sessions
 get('/chat/:friendId/start') do
-  id = session[:hejUser]
-  friendId=params[:friendId]
+     id = session[:hejUser]
+    friendId=params[:friendId]
  
-  db = connect_to_db("db/hej.db")
-  userResult = db.execute("SELECT * FROM user where id=?;", id)
-  user = userResult[0]
-#  allUsers = db.query("SELECT * FROM user;")
-  friendResult = db.execute("SELECT * FROM user where id=?;", friendId)
-  friend = friendResult[0]  
+     db = connect_to_db("db/hej.db")
+     userResult = db.execute("SELECT * FROM user where id=?;", id)
+     user = userResult[0]
+    friendResult = db.execute("SELECT * FROM user where id=?;", friendId)
+    friend = friendResult[0]  
 
-  chatResult = db.execute("SELECT * FROM chat where (user1=? and user2=?) or (user1=? and user2=?);", id,friendId, friendId,id)
-  if chatResult.length() > 0
-    chat = chatResult[0]
-  else
-    result = db.execute("SELECT MAX(id) as id FROM chat;")
-    first_result = result[0]
-    chatId = first_result['id'];
-    chatId = chatId + 1;
-    db.execute("INSERT INTO chat('id', 'starttime', 'user1', 'user2') VALUES (?, ?, ?, ?);",chatId,Time.now.to_time.to_i,id,friendId);
-    chatResult = db.execute("SELECT * FROM chat where user1=? and user2=?;", id,friendId)
-    if chatResult
-      chat = chatResult[0]
-    end  
-  end
-  showChatWindow(chat)
-# allUsers = db.execute("SELECT * FROM user;")
-# messageResult = db.execute("SELECT m.* FROM message m join chat c on m.chat=c.id where c.id=?;", chat["id]"])
-
-#  if friend
-#      slim(:chat, locals:{user:user,friend:friend,users:allUsers, messages:messageResult, chat:chat, showGrid:true})
-#  end
+    chatResult = db.execute("SELECT * FROM chat where (user1=? and user2=?) or (user1=? and user2=?);", id,friendId, friendId,id)
+    if chatResult.empty? 
+        #Ingen chat hittad för användarna
+        result = db.execute("SELECT MAX(id) as id FROM chat;")
+        if result.empty? || result[0]['id'] == nil
+            # Finns ingen chatt i databasen 
+            chatId = 1
+        else
+            # Skapa ny chat med första lediga id
+            first_result = result[0]
+            chatId = first_result['id'];
+            chatId = chatId + 1;
+        end 
+        db.execute("INSERT INTO chat('id', 'starttime', 'user1', 'user2') VALUES (?, ?, ?, ?);",chatId,Time.now.to_time.to_i,id,friendId);
+        chatResult = db.execute("SELECT * FROM chat where user1=? and user2=?;", id,friendId)
+        chat = chatResult[0]
+    else 
+     chat = chatResult[0]
+    end
+   
+    showChatWindow(chat)
 end
 
 enable :sessions
 post('/chat/:chatId/message') do
+    #spara nytt meddelande
     id = session[:hejUser]
     chatId=params[:chatId]
     message=params[:newMessage]
@@ -167,12 +167,15 @@ post('/chat/:chatId/message') do
     db = connect_to_db("db/hej.db") 
 
     result = db.execute("SELECT MAX(id) as id FROM message;")
-    first_result = result[0]
-    messageId = first_result['id']
-    if messageId != nil
-        messageId = messageId + 1
-    else
+    if result.empty? || result[0]['id'] == nil
+        # Inget message i db Använd id 1
         messageId = 1
+    else
+        # Skapa nytt message med första lediga id
+        first_result = result[0]
+        messageId = first_result['id']
+        messageId = messageId + 1
+   
     end 
 
     db.execute("INSERT INTO message('id', 'text', 'time', 'chat','user') VALUES (?, ?, ?, ?, ?);", messageId,message,Time.now.to_time.to_i,chatId, id)
